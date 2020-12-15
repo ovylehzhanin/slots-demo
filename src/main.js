56cd1355baf53a08106e3c9140e21fb1def8aa59
Oleksii Vylehzhanin <a.a.vylegzhanin@gmail.com>
2020-12-15 23:28:06 +0200

    wip: added js entry

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { getUserInfo, makeSpin } from './services.js';
+
+export function main() {
+  const $loginButton = $('#loginButton');
+
+  $loginButton.on('click', async () => {
+    console.log('fetch started');
+
+    let result = await getUserInfo('2000');
+    console.log('result', result);
+  });
+
+  const $makeSpinButton = document.querySelector('#makeSpinButton');
+
+  $makeSpinButton.addEventListener('click', async () => {
+    console.log(await makeSpin('2000', '20'));
+  }, false);
+};
