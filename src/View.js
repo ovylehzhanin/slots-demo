4950b5848563cbb6d621f81cfb1c3d5a44e54b83
Oleksii Vylehzhanin <a.a.vylegzhanin@gmail.com>
2020-12-19 19:30:05 +0200

    wip: added view class

---
+++ src/View.js
@@ -0,0 +1,42 @@
+const { _$, _$id } = window;
+
+export class View {
+  constructor(screens, rootSelector) {
+    this.screens = screens;
+    this.rootSelector = rootSelector;
+  }
+
+  get $root() {
+    return _$(this.rootSelector);
+  } 
+
+  clearRoot() {
+    this.$root.innerHTML = '';
+  }
+
+  unmountScreens() {
+    this.screens.forEach(screen => screen.unmount());
+  }
+
+  getRootContent() {
+    return this.$root.innerHTML;
+  }
+
+  setView(screenName) {
+    this.unmountScreens();
+
+    let rootContent = this.getRootContent();
+    if (rootContent) {
+      this.clearRoot();
+    }
+
+    this.screens
+      .find(screen => screen.name === screenName)
+      .mount();
+  }
+
+  showLoader() {
+    const $loaderDom = _$id('tmp.MainLoader').content.cloneNode(true);
+    this.$root.appendChild($loaderDom);
+  }
+}
