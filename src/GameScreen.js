627603255832ee6ea71a39f0c1e9e3c72663d301
Oleksii Vylehzhanin <a.a.vylegzhanin@gmail.com>
2020-12-21 08:11:05 +0200

    wip: game screen info update

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -5,4 +5,46 @@ export class GameScreen extends Screen {
   constructor(rootSelector, templateSelector) {
     super(rootSelector, templateSelector);
   }
+
+  get $spinButton() {
+    return this.$root.querySelector('#spinButton');
+  }
+
+  get $betInput() {
+    return this.$root.querySelector('#betInput');
+  }
+
+  get $balanceInput() {
+    return this.$root.querySelector('#balanceInput');
+  }
+
+  get $logoutButton() {
+    return this.$root.querySelector('#logoutButton');
+  }
+
+  initDetailsFetch() {
+    this.$root.dispatchEvent(new CustomEvent('game-screen-ready'));
+  }
+
+  updateDetails({ balance, last_bet, rolls }) {
+    this.$betInput.value = last_bet;
+    this.$balanceInput.value = balance;
+  }
+
+  bindEvents() {
+    this.$spinButton._on('click', (event) => {
+      const value = this.$betInput.value;
+      this.$root.dispatchEvent(new CustomEvent('spin-pressed', { detail: { value }}));
+    });
+
+    this.$logoutButton._on('click', (event) => {
+      this.$root.dispatchEvent(new CustomEvent('logout-pressed'));
+    });
+  }
+
+  mount() {
+    super.mount();
+    this.bindEvents();
+    this.initDetailsFetch();
+  }
 }
